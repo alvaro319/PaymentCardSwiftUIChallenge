f0f6665cca5e078399d832509c6174c21d166cc2
Alvaro Ordonez <alvaro_ordonez319@yahoo.com>
2025-08-07 23:18:13 -0400

    Initial Commit

---
+++ PaymentCardSwiftUIChallenge/PaymentCardSwiftUIChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaymentCardSwiftUIChallengeApp.swift
+//  PaymentCardSwiftUIChallenge
+//
+//  Created by Alvaro Ordonez on 8/7/25.
+//
+
+import SwiftUI
+
+@main
+struct PaymentCardSwiftUIChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
